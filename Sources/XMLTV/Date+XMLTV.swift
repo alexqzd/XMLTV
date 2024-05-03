5fa16841cd518f42ac27aad171fff6092097d03d
alexqzd <alexqm@icloud.com>
2024-05-03 11:15:01 -0600

    Hardcoded date locale for dateFormatter

--- Sources/XMLTV/Date+XMLTV.swift
+++ Sources/XMLTV/Date+XMLTV.swift
@@ -4,6 +4,7 @@ extension Date {
     static func parse(tvDate: String?) -> Date? {
         guard let tvDate = tvDate else { return nil }
         let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
         dateFormatter.dateFormat = "yyyyMMddHHmmss Z"
         return dateFormatter.date(from: tvDate)
     }
